72557602a26742e9d1b3885280a206094ea50ddc
Allen Sandiego <allen.sandiego@gmail.com>
2025-02-01 23:01:52 +0800

    Update readme, install script and auth script.

--- README.md
+++ README.md
@@ -1,13 +1,36 @@
 # Certbot-Auth
 
-### Required packages
+### Required package/s
 ```console
 curl
 ```
 
 ### Installation
-
+Command will download open-jdk-jre-21, wget package, certbot-auth jar file and shell script file.
 ```bash
 curl -sSL https://raw.githubusercontent.com/allensandiego/namecheap/refs/heads/main/certbot-auth/certbot-auth_install.sh | bash
 ```
 
+### Post install setup
+Before running the script, need to change some values in the certbot-auth.sh file.
+
+```bash
+# Get your API key from https://ap.www.namecheap.com/settings/tools
+API_USER="your-namecheap-username"
+API_KEY="your-namecheap-api-key"
+```
+
+### Running certbot 
+ Here's a sample on how to use script on certbot
+```bash
+sudo certbot certonly --manual --manual-auth-hook /opt/certbot-auth/certbot-auth.sh --preferred-challenges dns --non-interactive -d your.domain.com
+```
+
+### Disclaimer
+A few notes about the script.
+
+- Tested on Debian 12.
+- Tested on domains with less than 10 entries.
+- Generates response-{datetime}.xml listing all dns host entries in specified domain and request-{datetime}.txt showing the http get url on the install/application directory.
+
+Use at your own risk.
--- certbot-auth/certbot-auth_install.sh
+++ certbot-auth/certbot-auth_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Installation started."
+
 set -e
 
 appname=certbot-auth
@@ -9,19 +11,32 @@ if [ ! -d $appdir ]; then
   mkdir -m 755 $appdir
 fi
 
-cd $appdir
-
+echo "Install wget..."
 apt install wget
 
+echo "Installation directory... $appdir"
+cd $appdir
+
+echo "Download openjdk-jre-21..." 
 wget -O openjdk-jre-21.tar.gz https://builds.openlogic.com/downloadJDK/openlogic-openjdk-jre/21.0.3+9/openlogic-openjdk-jre-21.0.3+9-linux-x64.tar.gz
+
+echo "Unzip openjdk-jre-21..."
+tar -xv $appdir/openjdk-jre-21.tar.gz $appdir/
+
+chmod 755 -R $appdir/openjdk-jre-21
+
+echo "Download namecheap certbot files..."
 wget https://raw.githubusercontent.com/allensandiego/namecheap/refs/heads/main/certbot-auth/certbot-auth.sh
 wget https://github.com/allensandiego/namecheap/raw/refs/heads/main/certbot-auth/certbot-auth.jar
+wget https://raw.githubusercontent.com/allensandiego/namecheap/refs/heads/main/README.md
 
-tar -xv openjdk-jre-21.tar.gz
+chmod 755 $appdir/certbot-auth.sh
+chmod 755 $appdir/certbot-auth.jar
+chmod 755 $appdir/README.md
 
-chmod 755 -R openjdk-jre-21
-chmod 755 certbot-auth.sh
-chmod 755 certbot-auth.jar
+echo "Installation finished."
+
+cat $appdir/README.md
 
 
 
