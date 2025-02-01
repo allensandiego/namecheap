1782855c7e1030312c2d80f5d40ced1d8f3d9362
Allen Sandiego <allen.sandiego@gmail.com>
2025-02-01 22:00:41 +0800

    More fix install script

--- certbot-auth/certbot-auth_install.sh
+++ certbot-auth/certbot-auth_install.sh
@@ -11,9 +11,11 @@ fi
 
 cd $appdir
 
-curl -sSL https://builds.openlogic.com/downloadJDK/openlogic-openjdk-jre/21.0.3+9/openlogic-openjdk-jre-21.0.3+9-linux-x64.tar.gz | tar xz
+curl -sSL https://builds.openlogic.com/downloadJDK/openlogic-openjdk-jre/21.0.3+9/openlogic-openjdk-jre-21.0.3+9-linux-x64.tar.gz --output openjdk-jre-21.tar.gz
 curl -sSL https://raw.githubusercontent.com/allensandiego/namecheap/refs/heads/main/certbot-auth/certbot-auth.sh -output certbot-auth.sh
 curl -sSL https://github.com/allensandiego/namecheap/raw/refs/heads/main/certbot-auth/certbot-auth.jar -output certbot-auth.jar
+
+tar -xv openjdk-jre-21.tar.gz
 
 chmod 755 -R openjdk-jre-21
 chmod 755 certbot-auth.sh
