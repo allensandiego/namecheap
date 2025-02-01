8f1760de2701613323d6d8362f7edef2f386a0b0
Allen Sandiego <allen.sandiego@gmail.com>
2025-02-01 19:51:31 +0800

    Fix install script

--- certbot-auth/certbot-auth_install.sh
+++ certbot-auth/certbot-auth_install.sh
@@ -5,11 +5,18 @@ set -e
 appname=certbot-auth
 appdir=/opt/$appname
 
-mkdir -m 755 $appdir
+if [ ! -d $appdir ]l then
+  mkdir -m 755 $appdir
+fi
 
 cd $appdir
 
-curl -s https://github.com/allensandiego/namecheap/raw/refs/heads/main/certbot-auth/certbot-auth.jar
 curl -s https://raw.githubusercontent.com/allensandiego/namecheap/refs/heads/main/certbot-auth/certbot-auth.sh
+
+apt install wget
+
+wget https://github.com/allensandiego/namecheap/raw/refs/heads/main/certbot-auth/certbot-auth.jar
+
+chmod 755 certbot-auth.jar
 
 
